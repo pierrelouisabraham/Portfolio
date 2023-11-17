5fca639c4fcef0e00f17240035a2e29ffb83f677
pierrelouisabraham <pierrelouis.abraham@gmail.com>
2023-11-17 16:53:10 +0100

    first push

---
+++ src/components/Cv.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Cv() {
+    return (
+      <div className="main-cv">
+        <div className='title-cv'>
+            <h1 className=''></h1>
+            <img src='' alt=''/>
+        </div>
+        <div className='banner-body'>
+            <div className='banner'>
+
+            </div>
+            <div className='body'>
+                <div className='experience'>
+                    <div className='title-exp'>
+
+                    </div>
+                    <div className='tech'></div>
+                </div>
+                <div className='experience'>
+                    <div className='title-exp'>
+
+                    </div>
+                    <div className='tech'></div>
+                </div>
+                <div className='experience'>
+                    <div className='title-exp'>
+
+                    </div>
+                    <div className='tech'></div>
+                </div>
+                <div className='experience'>
+                    <div className='title-exp'>
+
+                    </div>
+                    <div className='tech'></div>
+                </div>
+            </div>
+
+        </div>
+      </div>
+    );
+  }
+  
+  export default Cv;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+      <div className="header">
+        <h1>Home</h1>
+        <h1>Portfolio</h1>
+        <h1>cv</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
